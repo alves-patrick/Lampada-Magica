0cba776325a2a724d54d981e732b33b784d39741
Patrick Alves <patrickalvesarts@gmail.com>
2023-08-09 14:53:36 -0300

    Initial Commit

---
+++ Lampada Magica/Lampada_MagicaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lampada_MagicaApp.swift
+//  Lampada Magica
+//
+//  Created by Patrick Alves on 09/08/23.
+//
+
+import SwiftUI
+
+@main
+struct Lampada_MagicaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
